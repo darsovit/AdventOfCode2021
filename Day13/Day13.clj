a410c7603c682f5faee89d147b11a69a201d56d7
darsovit <darsovit@gmail.com>
2022-01-01 13:58:51 -0700

    Advent of Code 2021, Day 13, Second Puzzle
    The solution to the second half puzzle of the Advent of Code 2021
    for day 13.
    
    https://adventofcode.com/2021/day/13

--- Day13/Day13.clj
+++ Day13/Day13.clj
@@ -31,7 +31,7 @@
                   foldmatches (re-matches #"^fold along ([xy])=(\d+)$" line)]
               (cond (not= nil dotmatches)  (let [[full xstr ystr] dotmatches
                                                  x (Day01/parse-int xstr)
-                                                 y (Day01/parse-int ystr)]       (update state :dots conj (list x y)))
+                                                 y (Day01/parse-int ystr)]       (update state :dots conj [x y]))
                     (not= nil foldmatches) (let [[full xy linestr] foldmatches
                                                  line (Day01/parse-int linestr)] (update state :folds conj (list xy line)))
                     :else  state
@@ -44,16 +44,16 @@
 (defn foldOnX
   "Given a dotset and an x-line, fold the dotset"
   [dotset xline]
-  (reduce (fn [ds pos] (cond (< xline (first pos)) (conj ds pos)
-                             :else                 (conj ds (list (- xline (- (first pos) xline)) (last pos)))
+  (reduce (fn [ds pos] (cond (> xline (first pos)) (conj ds pos)
+                             :else                 (conj ds [(- xline (- (first pos) xline)) (last pos)])
                              )
             ) #{} dotset))
 
 (defn foldOnY
   "Given a dotset and a y-line, fold the dotset"
   [dotset yline]
-  (reduce (fn [ds pos] (cond (< yline (last pos)) (conj ds pos)
-                             :else (conj ds (list (first pos) (- yline (- (last pos) yline))))               
+  (reduce (fn [ds pos] (cond (> yline (last pos)) (conj ds pos)
+                             :else (conj ds [(first pos) (- yline (- (last pos) yline))])               
                              )
             ) #{} dotset))
 
@@ -76,3 +76,27 @@
 (= 17 (CountDotsAfterOneFold testInput))
 )
 (CountDotsAfterOneFold puzzleInput)
+
+(defn performAllFolds
+  "Given an input, process the input and present the dotset after all folds are completed"
+  [inputs]
+  (let [puzzleState (buildStateFromInput inputs)
+        dotset (:dots puzzleState)
+        folds (:folds puzzleState)]
+    (reduce makeFoldOfTheDots dotset folds)))
+
+(sort (performAllFolds testInput))
+(defn visualizeDots
+ "Produce a visual representation of the dotset"
+ [dotset]
+ (let [maxX (+ 1 (first (apply max-key first dotset)))
+       maxY (+ 1 (second (apply max-key second dotset)))]
+   (reduce (fn [state value] (println value)) nil 
+   (loop [x 0 y 0 lines [] line []]
+     (cond (and (= y maxY) (= x maxX)) lines
+           (= x maxX) (recur 0 (inc y) (conj lines (clojure.string/join "" (conj line (if (contains? dotset [x y]) "#" " ")))) [])
+           :else      (recur (inc x) y lines (conj line (if (contains? dotset [x y]) "#" " "))))))
+ ))
+
+(apply max-key second (sort (performAllFolds testInput)))
+(visualizeDots (performAllFolds puzzleInput))
